2872d12e92f1df00afe851a23132d1e2b19045a7
Professor <Professor@PcMaker02>
2024-09-28 18:38:43 -0300

    Executando comandos DELETE e UPDATE

--- JavaWebDelete/src/java/javaBeans/Usuario.java
+++ JavaWebDelete/src/java/javaBeans/Usuario.java
@@ -42,5 +42,55 @@ public class Usuario extends Conectar {
         } catch (SQLException ex) {
             this.statusSQL = "Erro ao incluir usuario ! <br> " + ex.getMessage();
         }   
+    
+    
+    
+    
+    
+    
+    }
+    
+     public boolean deletar () {
+        try {
+            sql = "delete from usuarios where email = ? ";
+            ps = con.prepareStatement(sql); // prepara SQL
+            ps.setString(1, email); // Configura Parametros
+            ps.executeUpdate();
+            this.statusSQL = null; // armazena null se deu tudo certo
+        } catch (SQLException ex) {
+            this.statusSQL = "Erro ao tentar deletar Usuário! " + ex.getMessage();
+        }
+        return false;
+    }
+    
+     public boolean buscarEmail() {
+        try {
+            sql = "select * from usuarios where email = ? ";
+            ps = con.prepareStatement(sql); // prepara SQL
+            ps.setString(1, email); // Configura Parametros
+            tab = ps.executeQuery(); // Executa comando SQL
+            if (tab.next()) return true;
+            this.statusSQL = null; // armazena null se deu tudo certo
+        } catch (SQLException ex) {
+            this.statusSQL = "Erro ao tentar buscar Usuário! " + ex.getMessage();
+        }
+        return false;
+    }
+    
+    public void alterar(){
+       try { sql = "update usuarios set nome=?, email=?, celular=?,"
+                    + " senha=?, nivel=? where email=? ";
+            ps = con.prepareStatement(sql); // prepara SQL
+            ps.setString(1, nome); // Configura Parametros
+            ps.setString(2, email); // Configura Parametros
+            ps.setString(3, celular); // Configura Parametros
+            ps.setString(4, senha ); // Configura Parametros
+            ps.setString(5, nivel ); // Configura Parametros
+            ps.setString(6, email ); // Configura Parametros
+            ps.executeUpdate(); // executa comando SQL
+            this.statusSQL = null; // armazena null se deu tudo certo
+        } catch (SQLException ex) {
+            this.statusSQL = "Erro ao Alterar usuario ! <br> " + ex.getMessage();
+        }   
     }
 }
--- JavaWebDelete/web/javaJSP/registro.jsp
+++ JavaWebDelete/web/javaJSP/registro.jsp
@@ -1,20 +1,30 @@
 <%@page import="javaBeans.Usuario"%>
-<%  
-    Usuario user = new Usuario(); // Instancia o objeto Usuario 
-    if ( !(user.statusSQL == null) ) out.println(user.statusSQL);
-    
+<%  Usuario user = new Usuario(); // Instancia o objeto Usuario 
+    if (!(user.statusSQL == null)) { out.println(user.statusSQL); }
+
+    String oper = request.getParameter("oper");
     user.nome = request.getParameter("nome");
     user.email = request.getParameter("email");
     user.celular = request.getParameter("celular");
     user.senha = request.getParameter("senha");
     user.nivel = request.getParameter("nivel");
-    user.incluir();
-    if ( !(user.statusSQL == null) ) out.println(user.statusSQL);
-    else
-    {
-    
-      String sHTML="<center>Usuário criado com Sucesso!<br>"
-       + "<a href = '../index.html'> Voltar </a></center>";
-       out.println(sHTML);
-      }
- %>
+    String sHTML = "";
+
+    if (oper.equals("gravar")) {
+        if (user.buscarEmail()) {
+            user.alterar();
+            sHTML = "<center>Usuário Alterado com Sucesso!<br>"
+                    + "<a href = '../index.html'> Voltar </a></center>";
+        } else {
+            user.incluir();
+            sHTML = "<center>Usuário incluido com Sucesso!<br>"
+                    + "<a href = '../index.html'> Voltar </a></center>"; }
+    } else {
+        user.deletar();
+        sHTML = "<center>Usuário foi deletado com Sucesso!<br>"
+                + "<a href = '../index.html'> Voltar </a></center>"; }
+
+    if (!(user.statusSQL == null))
+        out.println(user.statusSQL);
+    else { out.println(sHTML); }
+%>
